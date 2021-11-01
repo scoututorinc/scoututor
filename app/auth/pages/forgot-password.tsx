bef74fe8f518c6407fc17c4e3a77816dd3aca5b2
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2021-11-01 22:57:45 +0000

    :construction: Adds main layout for the page
    Mobile responsivness still needs some work

--- app/auth/pages/forgot-password.tsx
+++ app/auth/pages/forgot-password.tsx
@@ -1,5 +1,5 @@
 import { BlitzPage, useMutation } from 'blitz'
-import Layout from 'app/core/layouts/Layout'
+import LoggedOutLayout from 'app/core/layouts/LoggedOutLayout'
 import { LabeledTextField } from 'app/core/components/LabeledTextField'
 import { Form, FORM_ERROR } from 'app/core/components/Form'
 import { ForgotPassword } from 'app/auth/validations'
@@ -43,6 +43,8 @@ const ForgotPasswordPage: BlitzPage = () => {
 }
 
 ForgotPasswordPage.redirectAuthenticatedTo = '/'
-ForgotPasswordPage.getLayout = (page) => <Layout title="Forgot Your Password?">{page}</Layout>
+ForgotPasswordPage.getLayout = (page) => (
+  <LoggedOutLayout title="Forgot Your Password?">{page}</LoggedOutLayout>
+)
 
 export default ForgotPasswordPage
--- app/auth/pages/signup.tsx
+++ app/auth/pages/signup.tsx
@@ -1,5 +1,5 @@
 import { useRouter, BlitzPage, Routes } from 'blitz'
-import Layout from 'app/core/layouts/Layout'
+import LoggedOutLayout from 'app/core/layouts/LoggedOutLayout'
 import { SignupForm } from 'app/auth/components/SignupForm'
 
 const SignupPage: BlitzPage = () => {
@@ -13,6 +13,6 @@ const SignupPage: BlitzPage = () => {
 }
 
 SignupPage.redirectAuthenticatedTo = '/'
-SignupPage.getLayout = (page) => <Layout title="Sign Up">{page}</Layout>
+SignupPage.getLayout = (page) => <LoggedOutLayout title="Sign Up">{page}</LoggedOutLayout>
 
 export default SignupPage
