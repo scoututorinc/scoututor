263d0c34b08a87de41bab0a8c29ed67bad298c9f
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2021-11-02 17:13:40 +0000

    :construction: Adds login page layout. No login logic implemented

--- app/auth/pages/login.tsx
+++ app/auth/pages/login.tsx
@@ -1,23 +1,28 @@
-import { useRouter, BlitzPage } from "blitz"
-import Layout from "app/core/layouts/Layout"
-import { LoginForm } from "app/auth/components/LoginForm"
+import { useRouter, BlitzPage } from 'blitz'
+import { Center } from '@chakra-ui/react'
+import Layout from 'app/core/layouts/Layout'
+import { LoginForm } from 'app/auth/components/LoginForm'
+import Navbar from '../../core/components/Navbar'
 
 const LoginPage: BlitzPage = () => {
   const router = useRouter()
 
   return (
     <div>
-      <LoginForm
-        onSuccess={() => {
-          const next = router.query.next ? decodeURIComponent(router.query.next as string) : "/"
-          router.push(next)
-        }}
-      />
+      <Navbar />
+      <Center>
+        <LoginForm
+          onSuccess={() => {
+            const next = router.query.next ? decodeURIComponent(router.query.next as string) : '/'
+            router.push(next)
+          }}
+        />
+      </Center>
     </div>
   )
 }
 
-LoginPage.redirectAuthenticatedTo = "/"
+LoginPage.redirectAuthenticatedTo = '/'
 LoginPage.getLayout = (page) => <Layout title="Log In">{page}</Layout>
 
 export default LoginPage
