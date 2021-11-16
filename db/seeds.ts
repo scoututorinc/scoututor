823911e54f2c1f221ccd646efda46848358fe3b2
ricardoslv <ricardoslv80@gmail.com>
2021-11-16 23:10:53 +0000

    :hammer: Add courses seed

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,13 +1,24 @@
 import { SecurePassword } from 'blitz'
-import db, { Prisma } from 'db'
-// import { Prisma } from '@prisma/client'
+import db, { Prisma, User } from 'db'
 import faker from 'faker'
 faker.locale = 'pt_PT'
 
 const range = (n: number) => [...new Array(n).keys()]
+const randomInt = (s: number, b: number) => Math.round(Math.random() * (b - s)) + b
 
 const seed = async () => {
+  await db.course.deleteMany({})
   await db.user.deleteMany({})
+
+  const users = await createUsers()
+  console.log(
+    `users`,
+    users.map((x) => x.id)
+  )
+  await createCourses(users)
+}
+
+async function createUsers() {
   const users: Prisma.UserCreateInput[] = []
 
   users.push({
@@ -17,7 +28,7 @@ const seed = async () => {
     hashedPassword: await SecurePassword.hash('passpass123')
   })
 
-  for (const _ in range(5)) {
+  for (const _ in range(10)) {
     const firstName = faker.name.firstName(1)
     const lastName = faker.name.lastName(1)
     users.push({
@@ -30,6 +41,28 @@ const seed = async () => {
 
   await db.user.createMany({
     data: users
+  })
+
+  return await db.user.findMany({})
+}
+
+async function createCourses(users: User[]) {
+  const courses: Prisma.CourseCreateManyInput[] = []
+
+  for (const _ in range(20)) {
+    courses.push({
+      name: faker.company.catchPhrase(),
+      description: faker.lorem.paragraphs(3, '.'),
+      icon: faker.image.technics(),
+      hourlyRate: randomInt(10, 20),
+      previewImages: range(randomInt(1, 4)).map((_) => faker.image.business()),
+      authorId: users[randomInt(1, 5)]?.id || 2
+    })
+  }
+
+  await db.course.createMany({
+    data: courses,
+    skipDuplicates: true
   })
 }
 
