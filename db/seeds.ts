4959c7e88d158dc673a9a7c24935e10bed839ab5
ricardoslv <ricardoslv80@gmail.com>
2021-11-17 16:57:37 +0000

    :hammer: Add membership and applications to seed

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,21 +1,20 @@
 import { SecurePassword } from 'blitz'
-import db, { Prisma, User } from 'db'
+import db, { Course, Prisma, User } from 'db'
 import faker from 'faker'
 faker.locale = 'pt_PT'
 
 const range = (n: number) => [...new Array(n).keys()]
-const randomInt = (s: number, b: number) => Math.round(Math.random() * (b - s)) + b
+const randomInt = (s: number, b: number) => Math.round(Math.random() * (b - s - 1)) + s
 
 const seed = async () => {
+  await db.courseMembership.deleteMany({})
   await db.course.deleteMany({})
   await db.user.deleteMany({})
 
   const users = await createUsers()
-  console.log(
-    `users`,
-    users.map((x) => x.id)
-  )
-  await createCourses(users)
+  const courses = await createCourses(users)
+  await createCourseMemberships(users, courses)
+  await createCourseApplications(users, courses)
 }
 
 async function createUsers() {
@@ -56,12 +55,50 @@ async function createCourses(users: User[]) {
       icon: faker.image.technics(),
       hourlyRate: randomInt(10, 20),
       previewImages: range(randomInt(1, 4)).map((_) => faker.image.business()),
-      authorId: users[randomInt(1, 5)]?.id || 2
+      authorId: users[randomInt(1, 5)]?.id || 0
     })
   }
 
   await db.course.createMany({
     data: courses,
+    skipDuplicates: true
+  })
+
+  return await db.course.findMany({})
+}
+
+async function createCourseMemberships(users: User[], courses: Course[]) {
+  const courseMemberships: Prisma.CourseMembershipCreateManyInput[] = []
+
+  for (const _ in range(20)) {
+    courseMemberships.push({
+      weeklyHours: randomInt(4, 8),
+      weeklySchedule: faker.lorem.paragraph(randomInt(1, 3)),
+      userId: users[randomInt(0, 5)]?.id || 0,
+      courseId: courses[randomInt(0, 14)]?.id || 0
+    })
+  }
+
+  await db.courseMembership.createMany({
+    data: courseMemberships,
+    skipDuplicates: true
+  })
+}
+
+async function createCourseApplications(users: User[], courses: Course[]) {
+  const courseApplications: Prisma.CourseApplicationCreateManyInput[] = []
+
+  for (const _ in range(20)) {
+    courseApplications.push({
+      description: faker.lorem.paragraphs(2),
+      availableSchedule: faker.lorem.paragraph(randomInt(1, 3)),
+      courseId: courses[randomInt(15, 19)]?.id || 0,
+      applicantId: users[randomInt(5, 10)]?.id || 0
+    })
+  }
+
+  await db.courseApplication.createMany({
+    data: courseApplications,
     skipDuplicates: true
   })
 }
