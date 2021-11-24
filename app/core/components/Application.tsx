df784ed68f3f90e9ee6b0079f38f0605015cf51f
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2021-11-24 17:28:46 +0000

    Adds the application managment screen for the tutor (#34)
    * :construction: Adds main layout for the application managment screen
    
    * :pencil2: Fixes typos

---
+++ app/core/components/Application.tsx
@@ -0,0 +1,72 @@
+import { FC } from 'react'
+import {
+  Flex,
+  Box,
+  HStack,
+  VStack,
+  Img,
+  Heading,
+  Divider,
+  Text,
+  Button,
+  Icon
+} from '@chakra-ui/react'
+import { AiFillMessage } from 'react-icons/ai'
+
+const Application: FC = () => {
+  return (
+    <Box borderWidth='1px' width='100%' rounded={6}>
+      <Flex direction='column' p={4}>
+        <HStack spacing={4} alignItems='center' justifyContent='start' mb={4}>
+          <Img
+            src='/images/knowledge.png'
+            alt='applicant'
+            borderRadius='full'
+            maxWidth='80px'
+          ></Img>
+          <Heading as='h5' size='md'>
+            Segun Adebayo
+          </Heading>
+        </HStack>
+        <VStack spacing={2} alignItems='start' mb={4}>
+          <Heading as='h6' size='md'>
+            Interest manifestation and questions
+          </Heading>
+          <Divider />
+          <Text fontWeight='bold'>
+            Hey there! I`ve wanted to learn the principles of graphical design. I`m currently a web
+            design, which means that I have a very close relationship with UI and UX designers.
+            Sometimes when we`re discussing design ideas for some projects I feel like I could use a
+            more formal and systematic knowledge of the area in order to provide more meaningful and
+            useful opinion. Furthermore, I`d like to contribute to the design process since I feel
+            some interesting raw ideas. I just wonder if by any chance we can work with the
+            following tools: Adobe Illustrator CS6, Adobe Photoshop CS6 and Figma.
+          </Text>
+        </VStack>
+        <VStack spacing={2} alignItems='start' mb={6}>
+          <Heading as='h6' size='md'>
+            Availability
+          </Heading>
+          <Divider />
+          <Text fontWeight='bold'>
+            <p>Tuesday: 11:00am - 12:00pm</p>
+            <p>Wednesday: 3:00pm - 6:00pm</p>
+            <p>Friday: 5:00pm - 6:00pm</p>
+          </Text>
+        </VStack>
+        <HStack justifyContent='center' mb={6}>
+          <Button colorScheme='teal'>Accept</Button>
+          <Button colorScheme='red'>Decline</Button>
+        </HStack>
+        <HStack justifyContent='center' mb={2}>
+          <Icon as={AiFillMessage}></Icon>
+          <Heading as='h6' size='sm'>
+            Follow up
+          </Heading>
+        </HStack>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Application
---
+++ app/pages/application_management.tsx
@@ -0,0 +1,58 @@
+import { BlitzPage, useSession } from 'blitz'
+import {
+  Flex,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  VStack,
+  HStack,
+  Heading,
+  Divider,
+  Box,
+  Img,
+  Text,
+  Button,
+  Icon
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { AiFillMessage } from 'react-icons/ai'
+import LoggedInLayout from 'app/core/layouts/LoggedInLayout'
+import Application from 'app/core/components/Application'
+
+const ApplicationManagement: BlitzPage = () => {
+  return (
+    <Flex
+      direction='column'
+      p={{ base: 6, md: 10 }}
+      width='100%'
+      overflowY='scroll'
+      overflowX='hidden'
+    >
+      <Breadcrumb spacing={4} pb={8} separator={<ChevronRightIcon color='gray.500' />}>
+        <BreadcrumbItem>
+          <BreadcrumbLink href=''>Courses</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href=''>Graphical Design by Bring Your own Laptop</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href=''>Application management</BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <VStack spacing={4} mb={6} alignItems='start'>
+        <Heading size='lg'>Applications management</Heading>
+        <Divider />
+      </VStack>
+      <VStack spacing={4}>
+        <Application />
+        <Application />
+      </VStack>
+    </Flex>
+  )
+}
+
+ApplicationManagement.suppressFirstRenderFlicker = true
+ApplicationManagement.getLayout = (page) => (
+  <LoggedInLayout title='Application management'>{page}</LoggedInLayout>
+)
+export default ApplicationManagement
