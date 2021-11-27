537446bf63ad261b2ad369710ab335f04fe2f3db
ricardoslv <ricardoslv80@gmail.com>
2021-11-27 19:30:15 +0000

    :children_crossing: Add routing to signup form cancel and navbar icon

--- app/core/components/Navbar.tsx
+++ app/core/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link as BlitzLink, Routes } from 'blitz'
 import { FC } from 'react'
 import { Box, Flex, Img, HStack, Input, Button, Link } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
+import { StyledLink } from 'app/core/components/StyledLink'
 
 const Navbar: FC = () => {
   return (
@@ -14,13 +15,16 @@ const Navbar: FC = () => {
         pl={10}
         pr={10}
       >
-        <Img
-          src='images/scoututor.png'
-          alt='scoututor'
-          maxHeight='80%'
-          maxWidth='20vh'
-          margin={{ base: '10px', lg: '0px' }}
-        />
+        <StyledLink href={Routes.Home().pathname}>
+          <Img
+            src='images/scoututor.png'
+            alt='scoututor'
+            maxHeight='80%'
+            maxWidth='20vh'
+            margin={{ base: '10px', lg: '0px' }}
+          />
+        </StyledLink>
+
         {/* TODO: If on mobile display we may not want to render this at all */}
         <HStack spacing={4} width={{ base: '90%', md: '40%' }} margin={{ base: '10px', lg: '0px' }}>
           <Input
