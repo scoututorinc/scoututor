c1084c2724fa07c29bd3612a6ab660b5c6ba7812
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2021-12-01 14:44:41 +0000

    :lipstick: Adds then Course Display component (#61)
    * :lipstick: Adds then Course Display component
    
    * :bug: Fixes 'id' type

--- app/courses/queries/getCourses.ts
+++ app/courses/queries/getCourses.ts
@@ -2,5 +2,7 @@ import { resolver } from 'blitz'
 import db from 'db'
 
 export default resolver.pipe(resolver.authorize(), async (_, ctx) => {
-  return await db.course.findMany({})
+  return await db.course.findMany({
+    include: { author: { select: { name: true, profilePicture: true } } }
+  })
 })
