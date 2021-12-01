8d197507d17b394981fabc55b732025ef44b05b0
hugo <hugofilipeduartecarvalho@gmail.com>
2021-12-01 01:18:39 +0000

    :sparkles: Implement Delete Account Button
    Implements the delete account feature

--- app/users/pages/profile.tsx
+++ app/users/pages/profile.tsx
@@ -2,12 +2,29 @@ import {
   BlitzPage,
   GetServerSideProps,
   invokeWithMiddleware,
-  InferGetServerSidePropsType
+  InferGetServerSidePropsType,
+  useRouter,
+  useMutation,
+  Routes
 } from 'blitz'
-import { Flex, Box } from '@chakra-ui/react'
+import {
+  AlertDialog,
+  Flex,
+  Box,
+  Button,
+  VStack,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay
+} from '@chakra-ui/react'
 
 import LoggedInLayout from 'app/core/layouts/LoggedInLayout'
 import getCurrentUser from 'app/users/queries/getCurrentUser'
+import React from 'react'
+import logout from 'app/auth/mutations/logout'
+import deleteAccount from 'app/auth/mutations/deleteAccount'
 
 import { PromiseReturnType } from 'next/dist/types/utils'
 
@@ -15,9 +32,55 @@ const Profile: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps>>
   currentUser,
   error
 }) => {
+  const router = useRouter()
+  const [logoutMutation] = useMutation(logout)
+  const [deleteAccMutation] = useMutation(deleteAccount)
+  const [isOpen, setIsOpen] = React.useState(false)
+  const onClose = () => setIsOpen(false)
+  const cancelRef = React.useRef()
   return currentUser ? (
     <Flex direction='column' w='100%' h='100%' overflowY='scroll' overflowX='hidden' p={10}>
-      <Box as='pre'>{JSON.stringify(currentUser, null, 2)}</Box>
+      <VStack>
+        <Box as='pre'>{JSON.stringify(currentUser, null, 2)}</Box>
+        <Button
+          type='submit'
+          colorScheme='red'
+          onClick={() => {
+            setIsOpen(true)
+          }}
+        >
+          Delete Account
+        </Button>
+        <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                Delete Account
+              </AlertDialogHeader>
+
+              <AlertDialogBody>
+                Are you sure? You cannot undo this action afterwards.
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button
+                  colorScheme='red'
+                  onClick={async () => {
+                    await deleteAccMutation()
+                    await logoutMutation()
+                    await router.push(Routes.Home())
+                  }}
+                >
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </VStack>
     </Flex>
   ) : (
     <p>{JSON.stringify(error)}</p>
