18d1d60b457d9a227e30de0293ecb6554dedc01a
ricardoSlv <47824386+ricardoSlv@users.noreply.github.com>
2021-12-03 13:57:44 +0000

    Update index.tsx

--- app/courses/pages/courses/[id]/index.tsx
+++ app/courses/pages/courses/[id]/index.tsx
@@ -22,7 +22,6 @@ const CourseView: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProp
 }) => {
   const is_enrolled = false
   const is_teacher = true
-  console.log(course, error)
   return course ? (
     <Flex direction='column' w='100%' h='100%' overflowY='scroll' overflowX='hidden' p={10}>
       <VStack spacing={2} pb={8} alignItems='start'>
