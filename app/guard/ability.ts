b537d615d83a4a668ba8035747ba7f12da9cbe95
ricardoslv <ricardoslv80@gmail.com>
2021-12-11 21:18:52 +0000

    :sparkles: Add access control checks for course page

--- app/guard/ability.ts
+++ app/guard/ability.ts
@@ -1,33 +1,37 @@
 import db, { Prisma } from 'db'
 import { GuardBuilder } from '@blitz-guard/core'
+import { AuthenticatedMiddlewareCtx } from 'blitz'
 
-type ExtendedResourceTypes = 'comment' | 'article' | Prisma.ModelName
+type ExtendedResourceTypes = Prisma.ModelName
 
-type ExtendedAbilityTypes = 'send email'
+type ExtendedAbilityTypes = 'join'
 
 const Guard = GuardBuilder<ExtendedResourceTypes, ExtendedAbilityTypes>(
   async (ctx, { can, cannot }) => {
     cannot('manage', 'all').reason('You did something wrong!')
-    /*
-		Your rules go here, you can start by removing access to everything
-		and gradually adding the necessary permissions
 
-		eg:
-		cannot("manage", "article")
+    if (ctx.session.$isAuthorized()) {
+      const authCtx = ctx as AuthenticatedMiddlewareCtx
 
-		can("read", "article")
-		can("read", "comment")
+      can('update', 'Course', async (args) => {
+        return (
+          (await db.course.count({
+            where: { id: args.id, authorId: authCtx.session.userId }
+          })) === 1
+        )
+      })
 
-		if (ctx.session.isAuthorized()) {
-			can("create", "article")
-			can("create", "comment")
-			can("send email", "comment")
-
-			can("delete", "comment", async (_args) => {
-				return (await db.comment.count({ where: { userId: ctx.session.userId } })) === 1
-			})
-		}
-    */
+      can('join', 'Course', async (args: { id: number }) => {
+        return (
+          (await db.courseMembership.count({
+            where: { courseId: args.id, userId: authCtx.session.userId }
+          })) === 0 &&
+          (await db.course.count({
+            where: { id: args.id, authorId: authCtx.session.userId }
+          })) === 0
+        )
+      })
+    }
   }
 )
 
