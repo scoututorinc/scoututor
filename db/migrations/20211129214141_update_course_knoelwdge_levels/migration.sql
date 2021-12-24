7648122b99e2e34b9883a4765714a87045abf8e6
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2021-12-24 01:01:42 +0000

    :bulb: Removes duplicate 'location' field and useless migrations

--- db/migrations/20211129214141_update_course_knoelwdge_levels/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `levels` on the `Course` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Course" DROP COLUMN "levels",
-ADD COLUMN     "knowledgeLevels" "KnowledgeLevel"[];
--- db/migrations/20211201153928_set_dault_status_for_course/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Course" ALTER COLUMN "status" SET DEFAULT E'HIDDEN';
