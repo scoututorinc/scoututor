8a9bb0b4dac529fc19f1a210bf0959e1885c066a
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-01-17 13:34:21 +0000

    Adds action to course review submission button (#100)

--- app/courses/pages/courses/[id]/index.tsx
+++ app/courses/pages/courses/[id]/index.tsx
@@ -1,12 +1,29 @@
+import React, { useState } from 'react'
 import {
+  useMutation,
   Routes,
   BlitzPage,
   GetServerSideProps,
   invokeWithMiddleware,
   InferGetServerSidePropsType
 } from 'blitz'
-import { Flex, Heading, Divider, VStack, Button } from '@chakra-ui/react'
-
+import {
+  Flex,
+  Heading,
+  Divider,
+  VStack,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Img,
+  HStack
+} from '@chakra-ui/react'
+import { Form } from 'app/core/components/forms/Form'
 import { PromiseReturnType } from 'next/dist/types/utils'
 
 import LoggedInLayout from 'app/core/layouts/LoggedInLayout'
@@ -17,13 +34,30 @@ import { StyledLink } from 'app/core/components/StyledLink'
 
 import getCourse from 'app/courses/queries/getCourse'
 import getAbility from 'app/guard/queries/getAbility'
+import createCourseReview from 'app/courses/mutations/createCourseReview'
+import { CreateCourseReview } from 'app/courses/validations'
 import { paramToInt } from 'utils'
+import { LabeledTextAreaField } from 'app/core/components/forms/LabeledTextAreaField'
+import { LabeledTextField } from 'app/core/components/forms/LabeledTextField'
 
 const CourseView: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({
   course,
   error,
   permissions
 }) => {
+  const [commentModalIsOpen, setCommentModalIsOpen] = useState(false)
+  const onCloseCommentModal = () => setCommentModalIsOpen(false)
+
+  const [submittedReviewModal, setSubmittedReviewModal] = useState({ status: false, text: 0 })
+  const onCloseSubmittedReviewModal = () => setSubmittedReviewModal({ status: false, text: 0 })
+
+  const resultModalText = [
+    '✅ Your review was sucessfully submitted',
+    '❌ Something went wrong while submitting your review'
+  ]
+
+  const [createCourseReviewMutation] = useMutation(createCourseReview)
+
   return course && permissions ? (
     <Flex direction='column' w='100%' h='100%' overflowY='scroll' overflowX='hidden' p={10}>
       <VStack spacing={2} pb={8} alignItems='start'>
@@ -68,12 +102,60 @@ const CourseView: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProp
                   See all course posts
                 </Button>
               </StyledLink> */}
-              <Button colorScheme='teal' width='80%'>
+              <Button colorScheme='teal' width='80%' onClick={() => setCommentModalIsOpen(true)}>
                 Comment on course
               </Button>
+              <Modal isOpen={commentModalIsOpen} onClose={onCloseCommentModal}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>
+                    <HStack w='100%' justifyContent='center'>
+                      <Img src='/images/commentary.png' maxW='100px' />
+                      <Heading as='h3' size='md'>
+                        Comment on the course tutor
+                      </Heading>
+                    </HStack>
+                  </ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody>
+                    <Form
+                      schema={CreateCourseReview}
+                      initialValues={{ classification: 1, comment: '', courseId: course.id }}
+                      onSubmit={async (values) => {
+                        try {
+                          await createCourseReviewMutation(values)
+                          setCommentModalIsOpen(false)
+                          setSubmittedReviewModal({ status: true, text: 0 })
+                        } catch (e) {
+                          console.log(e)
+                          setSubmittedReviewModal({ status: true, text: 1 })
+                        }
+                      }}
+                    >
+                      <VStack spacing={2}>
+                        <LabeledTextField type='number' label='Rating' name='classification' />
+                        <LabeledTextAreaField label='Comment' name='comment' mb={2} />
+
+                        <Button colorScheme='teal' type='submit'>
+                          Submit
+                        </Button>
+                      </VStack>
+                    </Form>
+                  </ModalBody>
+                </ModalContent>
+              </Modal>
               <Button colorScheme='red' width='80%'>
                 Cancel subscription
               </Button>
+              <Modal isOpen={submittedReviewModal.status} onClose={onCloseSubmittedReviewModal}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalBody>
+                    <Heading size='md'>{resultModalText[submittedReviewModal.text]}</Heading>
+                  </ModalBody>
+                  <ModalCloseButton />
+                </ModalContent>
+              </Modal>
             </VStack>
           )}
         </Flex>
