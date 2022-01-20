420812bee06283cd4baff6e269ef725719842165
ricardoSlv <47824386+ricardoSlv@users.noreply.github.com>
2022-01-20 20:22:06 +0000

    Allow student to cancel his course membership (#102)
    * Start
    
    * Finished
    
    * Fix

--- app/users/components/Notification.tsx
+++ app/users/components/Notification.tsx
@@ -1,8 +1,22 @@
 import { Box, Heading, Button, Img, HStack } from '@chakra-ui/react'
-import { useMutation } from 'blitz'
+import { useMutation, useQuery } from 'blitz'
 import dismissNotification from 'app/users/mutations/dismissNotification'
+import getUser from '../queries/getUser'
 
-const notificationTypeToReadable = (type) => {
+interface notifProps {
+  course: {
+    id: number
+    title: string
+    previewImage: string
+  }
+  id: number
+  createdAt: Date
+  type: string
+  entityId: number
+  onDismiss: () => void
+}
+
+const notificationTypeToReadable = (type: string, entityName?: string) => {
   switch (type) {
     case 'APPLICATION_CREATE':
       return 'An application has been submitted'
@@ -16,25 +30,31 @@ const notificationTypeToReadable = (type) => {
       return 'A follow up comment has been submitted'
     case 'MEMBERSHIP_CANCEL':
       return 'A membership has been canceled'
+    case 'MEMBERSHIP_LEAVE':
+      return (entityName ? entityName : 'A student') + ' has canceled his membership'
   }
 }
-const Notification = (props) => {
+const Notification = ({ course, id, createdAt, type, entityId, onDismiss }: notifProps) => {
   const [dismissNotificationMutation] = useMutation(dismissNotification)
+  const [user] = useQuery(getUser, entityId, {
+    suspense: false,
+    enabled: type == 'MEMBERSHIP_LEAVE'
+  })
 
   return (
     <Box borderWidth='2px' borderRadius={6} w='100%'>
       <HStack w='100%' px={4} py={2} justifyContent='space-between'>
         <HStack>
-          <Img src={props.notif.course.previewImage} maxW='75px' borderRadius='full' />
+          <Img src={course.previewImage} maxW='75px' borderRadius='full' />
           <Heading as='h2' size='sm'>
-            {props.notif.course.title} - {notificationTypeToReadable(props.notif.type)}
+            {course.title} - {notificationTypeToReadable(type, user?.name)}
           </Heading>
         </HStack>
         <Button
           colorScheme='red'
           onClick={async () => {
-            dismissNotificationMutation(props.notif.id)
-            props.onDismiss()
+            dismissNotificationMutation(id)
+            onDismiss()
           }}
         >
           Dismiss
